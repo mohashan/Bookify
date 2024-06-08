41d5dcb7128570e292f5059cd4d6d7dd9f9ec222
mohashan <msh200x@gmail.com>
2024-06-08 21:18:47 +0330

    Add Dapper

---
+++ src/Bookify.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Abstractions.Email;
+public interface IEmailService
+{
+    Task SendAsync(Domain.Users.Email email, string subject, string body);
+}
